1ca91cbb72a267d28cc4832adf5329103a05d265
jargonfreetechnology <jargonfreetechnology@gmail.com>
2020-10-30 04:28:35 +0530

    message.propertied

--- src/main/java/com/ms/main/ReadMsApplication.java
+++ src/main/java/com/ms/main/ReadMsApplication.java
@@ -7,14 +7,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import brave.sampler.Sampler;
+
 @SpringBootApplication
-@EnableDiscoveryClient
 @ComponentScan({"com.acontroller", "com.bservice"})
 @EnableJpaRepositories("com.cdao")
 @EntityScan("com.domain")
+@EnableDiscoveryClient
 public class ReadMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadMsApplication.class, args);
+	}
+
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
